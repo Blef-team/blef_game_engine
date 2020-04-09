34c91cabf750035cf1aa6c2473ee3d40ff8b3d99
maciej-pomykala <hobixpl@gmail.com>
2020-04-10 00:38:10 +0100

    Make sure appropriate packages are installed

--- api/run_api.R
+++ api/run_api.R
@@ -1,1 +1,7 @@
-plumber::plumb("./api/endpoints.R")$run(port = 8000)
+if(!require(plumber)) install.packages("plumber")
+if(!require(uuid)) install.packages("uuid")
+if(!require(magrittr)) install.packages("magrittr")
+if(!require(readr)) install.packages("readr")
+if(!require(dplyr)) install.packages("dplyr")
+
+plumb("./api/endpoints.R")$run(port = 8000)
