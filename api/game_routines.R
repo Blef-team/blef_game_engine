2b3c7b889d90c8d9b6aabeffe0011bf334baf2f4
Adrian Golian <adix9512@gmail.com>
2020-04-10 12:50:01 +0200

    Move indexation.csv to api/

--- api/game_routines.R
+++ api/game_routines.R
@@ -1,15 +1,15 @@
-indexation <- read_csv("indexation.csv")
+indexation <- read_csv("./api/indexation.csv")
 
 get_path <- function(game_uuid, r = -1) {
   if (r == -1) return(paste0("./game_data/", game_uuid, ".RDS"))
   if (r >= 0) return(paste0("./game_data/", game_uuid, "_", r, ".RDS"))
-} 
+}
 
 draw_cards <- function(players) {
   possible_cards <- expand.grid(0:5, 0:3)
-  all_cards <- possible_cards[sample(1:24, sum(players$n_cards)), ] 
+  all_cards <- possible_cards[sample(1:24, sum(players$n_cards)), ]
   nicknames <- lapply(1:nrow(players), function(p) rep(players$nickname[p], players$n_cards[p])) %>% unlist()
-  cbind(nicknames, all_cards) %>% 
+  cbind(nicknames, all_cards) %>%
     as.data.frame() %>%
     set_colnames(c("player", "value", "colour")) %>%
     mutate(player = as.character(player), value = as.numeric(value), colour = as.numeric(colour)) %>%
@@ -18,7 +18,7 @@ draw_cards <- function(players) {
 
 jsonise_hands <- function(game, nicknames = unique(game$players$nickname)) {
   lapply(nicknames, function(p) {
-    list(Nickname = p, Hand = game$hands %>% filter(player == p) %>% select(-player)) 
+    list(Nickname = p, Hand = game$hands %>% filter(player == p) %>% select(-player))
   })
 }
 
@@ -28,10 +28,10 @@ determine_set_existence <- function(cards, set_id) {
   set_type <- unlist(indexation[set_id + 1, 1])
   detail_1 <- unlist(indexation[set_id + 1, 2])
   detail_2 <- unlist(indexation[set_id + 1, 3])
-  
+
   card_values <- cards[, 2]
   card_colours <- cards[, 3]
-  
+
   if (set_type == "High card") {
     outcome <- sum(card_values == detail_1) >= 1
     return(outcome)
@@ -48,7 +48,7 @@ determine_set_existence <- function(cards, set_id) {
     outcome <- any(card_values == 1) & any(card_values == 2) & any(card_values == 3) & any(card_values == 4) & any(card_values == 5)
     return(outcome)
   } else if (set_type == "Great straight") {
-    outcome <- any(card_values == 0) & any(card_values == 1) & any(card_values == 2) & any(card_values == 3) & any(card_values == 4) & any(card_values == 5)    
+    outcome <- any(card_values == 0) & any(card_values == 1) & any(card_values == 2) & any(card_values == 3) & any(card_values == 4) & any(card_values == 5)
     return(outcome)
   } else if (set_type == "Three of a kind") {
     outcome <- sum(card_values == detail_1) >= 3
@@ -93,4 +93,4 @@ format_history <- function(history) {
   history %>%
     set_colnames(c("player", "action_id")) %>%
     mutate(player = as.character(player), action_id = as.numeric(action_id))
-} 
+}
