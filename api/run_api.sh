f12ebb8a6ac5598905b4abd64ae58740a956e619
Adrian Golian <adix9512@gmail.com>
2020-04-10 20:29:44 +0200

    Let the deployment continue after starting api

--- api/run_api.sh
+++ api/run_api.sh
@@ -1,5 +1,3 @@
 cd /var/gameengineservice/api/
-touch codedeploy_started_run_api
 mkdir -p game_data
-nohup Rscript run_api.R &
-touch codedeploy_finished_run_api
+Rscript run_api.R > /dev/null 2> /dev/null < /dev/null &
