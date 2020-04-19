781c28dcc5f3712ac10fcbc781b993c9ac0b8131
Adrian Golian <adix9512@gmail.com>
2020-04-19 17:29:28 +0200

    Add s3 module to handle s3 connections

---
+++ api/s3.R
@@ -0,0 +1,16 @@
+library("aws.ec2metadata")
+
+s3_access_key = metadata$iam_role("CodeDeploy-EC2-Instance-Profile")$AccessKeyId
+s3_secret_access_key = metadata$iam_role("CodeDeploy-EC2-Instance-Profile")$SecretAccessKey
+
+readRDS <- function(path) {
+  s3readRDS(object=path, bucket="game-data", key=s3_access_key, secret=s3_secret_access_key)
+}
+
+saveRDS <- function(game, path) {
+  s3readRDS(object=path, bucket="game-data", key=s3_access_key, secret=s3_secret_access_key)
+}
+
+game_exists <- function(path) {
+  return(object_exists(object=path, bucket="game-data", key=s3_access_key, secret=s3_secret_access_key))
+}
