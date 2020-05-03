d3004f2f9f6d8101d03974b533d6302477f0d77f
Adrian Golian <adix9512@gmail.com>
2020-05-03 22:17:50 +0200

    Fix deployment paths

--- api/run_api.sh
+++ api/run_api.sh
@@ -1,6 +1,6 @@
-cd /var/gameengineservice/api/
+cd /var/gameengineservice/api/v2/
 export GAME_DATA_PATH=~/game_data/v2/
 mkdir -p $GAME_DATA_PATH
 touch ~/api_v2.log
 echo $(date -u) >> ~/api_v2.log
-Rscript --verbose api/run_api.R >> ~/api_v2.log 2>&1 &
+Rscript --verbose run_api.R >> ~/api_v2.log 2>&1 &
