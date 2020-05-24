3bf767eb0123e7a3d1e4eaee0a9f2c4e70f4f89c
Adrian Golian <adix9512@gmail.com>
2020-05-24 21:08:11 +0200

    Add test for games/{id} endpoint for running games

--- api/test/testing_utils.py
+++ api/test/testing_utils.py
@@ -1,6 +1,16 @@
 import os
 from uuid import UUID
 
+n_players_to_max_cards = {
+    2: 11,
+    3: 8,
+    4: 6,
+    5: 4,
+    6: 4,
+    7: 3,
+    8: 3
+}
+
 
 def test_games(response):
     """ Test the /games endpoint response"""
@@ -33,6 +43,27 @@ def test_game_state(response):
     assert isinstance(game_state.get("hands"), list)
     assert "cp_nickname" in game_state
     assert isinstance(game_state.get("history"), list)
+
+
+def test_running_game_state_with_player_uuid(response):
+    test_game_state(response)
+    game_state = response.json()
+    assert isinstance(game_state.get("admin_nickname"), str) and game_state.get("admin_nickname")
+    assert game_state.get("status") == "Running"
+    assert game_state.get("round_number") in range(1, 24)
+    assert game_state.get("players")
+    n_players = len(game_state.get("players"))
+    assert game_state.get("max_cards") == n_players_to_max_cards[n_players]
+    for hand_player in game_state.get("hands"):
+        assert isinstance(hand_player.get("nickname"), str) and hand_player.get("nickname")
+        assert isinstance(hand_player.get("hand"), list) and hand_player.get("hand")
+        for card in hand_player.get("hand"):
+            assert card.get("value") in range(6)
+            assert card.get("colour") in range(4)
+    assert isinstance(game_state.get("cp_nickname"), str) and game_state.get("cp_nickname")
+    for action in game_state.get("history"):
+        assert isinstance(action["player"], str) and action["player"]
+        assert action["action_id"] in range(90)
 
 
 def test_join(response):
