d9eda1d507cf95f34745a6dff113b9b5c90e28ab
Adrian Golian <adix9512@gmail.com>
2020-05-25 01:52:30 +0200

    Update validation scripts to match new workflow

--- deployment/validate_dev.sh
+++ deployment/validate_dev.sh
@@ -1,9 +1,11 @@
-# Validation script to ensure develop deployments are NOT deployed on ports 8000,8001,8002
-# and are exclusively minor-versioned in the endpoints' URL paths (v2.0, but no v2)
-if grep -q 'plumber::plumb("endpoints.R")$run(port = 8000,\|plumber::plumb("endpoints.R")$run(port = 8001,\|plumber::plumb("endpoints.R")$run(port = 8002,' api/run_api.R; then
-  exit 1
-elif grep -Eq '#* @get /v[0-9]+\.[0-9]+' api/endpoints.R; then
-  exit 0
+# Validation script to ensure develop deployments are deployed on port 8020
+# and are exclusively major-versioned in the endpoints' URL paths (/v2/ but no /v2.4.2/)
+if grep -q 'plumber::plumb("endpoints.R")$run(port = 8020,' api/run_api.R; then
+  if grep -Eq '#* @get /v[0-9]+\.[0-9]+' api/endpoints.R; then
+    exit 1
+  else
+    exit 0
+  fi
 else
   exit 1
 fi
