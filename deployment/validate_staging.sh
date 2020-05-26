044279215630ab6edc379754062d7e996d2f850f
Adrian Golian <adix9512@gmail.com>
2020-05-26 14:20:59 +0200

    Fix validation scripts

--- deployment/validate_staging.sh
+++ deployment/validate_staging.sh
@@ -1,6 +1,6 @@
 # Validation script to ensure staging deployments are deployed on ports 8010,8011,8012
 # and are exclusively major-versioned in the endpoints' URL paths (/v2/, but no /v2.1/)
-if grep -q 'plumber::plumb("endpoints.R")$run(port = 8010,\|plumber::plumb("endpoints.R")$run(port = 8011,\|plumber::plumb("endpoints.R")$run(port = 8012,' api/run_api.R; then
+if grep -q 'PORT=8010 Rscript --verbose run_api.R\|PORT=8011 Rscript --verbose run_api.R\|PORT=8012 Rscript --verbose run_api.R' api/run_api.sh; then
   if grep -Eq '#* @get /v[0-9]+\.[0-9]+' api/endpoints.R; then
     exit 1
   else
