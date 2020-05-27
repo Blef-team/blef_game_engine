8b4dc7cc6f0de1d3c7f93891fcb414c6b7ca29b2
Adrian Golian <adix9512@gmail.com>
2020-05-27 22:21:42 +0200

    Kill previous process at PORT before run_api.R

--- api/run_api.sh
+++ api/run_api.sh
@@ -4,12 +4,13 @@ export GAME_DATA_PATH=~/game_data/v2/
 mkdir -p $GAME_DATA_PATH
 touch ~/api_v2.3.log
 echo $(date -u) >> ~/api_v2.3.log
+export PORT=8020
 if [ "$DEPLOYMENT_GROUP_NAME" == "GameEngineService-Production" ]
 then
     PORT=8001 Rscript --verbose run_api.R >> ~/api_v2.3.log 2>&1 &
 elif [ "$DEPLOYMENT_GROUP_NAME" == "GameEngineService-Staging" ]
 then
     PORT=8011 Rscript --verbose run_api.R >> ~/api_v2.3.log 2>&1 &
-else
-    PORT=8020 Rscript --verbose run_api.R >> ~/api_v2.3.log 2>&1 &
 fi
+lsof -i :$PORT -sTCP:LISTEN |awk 'NR > 1 {print $2}'  |xargs kill -15
+Rscript --verbose run_api.R >> ~/api_v2.3.log 2>&1 &
