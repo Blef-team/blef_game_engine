8ef7000e25ac5c4b258ffdf15db1b5dae7f22f54
Adrian Golian <adix9512@gmail.com>
2020-05-27 02:38:15 +0200

    Correct run_api.R to run_api.sh

--- deployment/validate_dev.sh
+++ deployment/validate_dev.sh
@@ -1,6 +1,6 @@
 # Validation script to ensure develop deployments are deployed on port 8020
 # and are exclusively major-versioned in the endpoints' URL paths (/v2/ but no /v2.4.2/)
-if grep -q 'PORT=8020 Rscript --verbose run_api.R' api/run_api.R; then
+if grep -q 'PORT=8020 Rscript --verbose run_api.R' api/run_api.sh; then
   if grep -Eq '#* @get /v[0-9]+\.[0-9]+' api/endpoints.R; then
     exit 1
   else
