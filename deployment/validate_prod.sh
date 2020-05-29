e179c2b0e4795bb0e7ab37805405ab2152536c02
Adrian Golian <adix9512@gmail.com>
2020-05-29 19:22:35 +0200

    Update validation scipts

--- deployment/validate_prod.sh
+++ deployment/validate_prod.sh
@@ -1,6 +1,6 @@
 # Validation script to ensure production deployments are deployed on ports 8000,8001,8002
 # and are exclusively major-versioned in the endpoints' URL paths (v2, but no v2.1)
-if grep -q 'PORT=8000 Rscript --verbose run_api\.R\|PORT=8001 Rscript --verbose run_api\.R\|PORT=8002 Rscript --verbose run_api\.R' api/run_api.sh; then
+if grep -q 'export PORT=8000\|export PORT=8001\|export PORT=8002' api/run_api.sh; then
   if grep -Eq '#* @get /v[0-9]+\.[0-9]+' api/endpoints.R; then
     exit 1
   else
