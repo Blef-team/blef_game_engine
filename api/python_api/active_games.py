03ac39086c5d3f3858129f2df9eb7bb62e9a188f
Adrian Golian <adix9512@gmail.com>
2021-06-21 20:23:36 +0300

    Add active_games

---
+++ api/python_api/active_games.py
@@ -0,0 +1,19 @@
+import boto3
+from boto3.dynamodb.conditions import Attr
+import time
+
+
+def query_dynamodb():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("games")
+    now = round(time.time())
+    diff = 3600
+    response = table.scan(
+        FilterExpression=Attr('last_modified').gt(now - diff)
+    )
+    return response['Items']
+
+
+def lambda_handler(event, context):
+    items = query_dynamodb()
+    return {"active_games": len(items)}
