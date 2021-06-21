1973b566dbf5b09f30d1eede265837e4ce24d72f
Adrian Golian <adix9512@gmail.com>
2021-06-21 20:23:10 +0300

    Add make_public

---
+++ api/python_api/make_public.py
@@ -0,0 +1,137 @@
+import uuid
+import boto3
+from boto3.dynamodb.conditions import Key
+import time
+import json
+from itertools import islice, product
+
+
+def response_payload(status_code, body):
+    return {
+            'statusCode': status_code,
+            'body': json.dumps(body),
+            'headers': {
+                'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-api-key,X-Amz-Security-Token',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+                'Access-Control-Allow-Credentials' : True,
+                'Content-Type': 'application/json'
+            },
+        }
+
+
+def internal_error_payload(err, message=None):
+    body = "Internal Lambda function error: {}".format(err)
+    if message:
+        body = "{}\n{}".format(body, message)
+    return response_payload(500, body)
+
+
+def request_error_payload(request, message=None):
+    body = "Bad request payload: '{}'".format(request)
+    if message:
+        body = "{}\n{}".format(body, message)
+    return response_payload(400, body)
+
+
+def parameter_error_payload(param_key, param_value, message=None):
+    body = "Bad input value in '{}': {}".format(param_key, param_value)
+    if message:
+        body = "{}\n{}".format(body, message)
+    return response_payload(400, body)
+
+
+def parse_event(event):
+    # Basic input validation
+    if not isinstance(event, dict):
+        return False
+
+    # Handle both direct triggers and API Gateway
+    body = event.get("body", event)
+    if isinstance(body, str):
+        try:
+            body = json.loads(body)
+        except ValueError:
+            return None
+    return body
+
+
+def is_valid_uuid(value):
+    try:
+        uuid.UUID(str(value))
+        return True
+    except ValueError:
+        return False
+
+
+def get_from_dynamodb(game_uuid):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("games")
+    response = table.query(KeyConditionExpression=Key('game_uuid').eq(game_uuid))
+    items = response.get("Items")
+    if len(items) == 1:
+        return items[0]
+    return None
+
+
+def update_in_dynamodb(game_uuid, public):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("games")
+    table.update_item(
+        Key={
+            'game_uuid': game_uuid
+        },
+        UpdateExpression="set last_modified = :last_modified, #game_public = :public",
+        ExpressionAttributeValues={
+            ':last_modified': round(time.time()),
+            ':public': public
+        },
+        ExpressionAttributeNames={
+            '#game_public': "public"
+        },
+        ReturnValues="NONE"
+    )
+    return True
+
+
+def lambda_handler(event, context):
+    try:
+        body = parse_event(event)
+        if not body:
+            return request_error_payload(event)
+
+        game_uuid = str(body.get("game_uuid"))
+        if not is_valid_uuid(game_uuid):
+            return parameter_error_payload("game_uuid", game_uuid, message="Invalid game UUID")
+
+        game = get_from_dynamodb(game_uuid)
+        if not game:
+            return parameter_error_payload("game_uuid", game_uuid, message="Game does not exist")
+
+        if game.get("status") != "Not started":
+            return response_payload(403, "Cannot make the change - game already started")
+
+        admin_uuid = str(body.get("admin_uuid"))
+
+        if not admin_uuid:
+            return parameter_error_payload("admin_uuid", admin_uuid, message="Admin UUID missing - please supply it")
+
+        if not is_valid_uuid(admin_uuid):
+            return parameter_error_payload("admin_uuid", admin_uuid, message="Invalid admin UUID")
+
+        players = game.get("players")
+        game_admin_uuid = [player["uuid"] for player in players if player["nickname"] == game.get("admin_nickname")][0]
+        if game_admin_uuid != admin_uuid:
+            return parameter_error_payload("admin_uuid", admin_uuid, message="Admin UUID does not match")
+
+        if game.get("public") == True:
+            return response_payload(200, "Request redundant - game already public")
+
+        public = True
+
+        update_in_dynamodb(game_uuid, public)
+
+        return response_payload(200, "Game made public")
+
+    except Exception as err:
+        return internal_error_payload(err)
